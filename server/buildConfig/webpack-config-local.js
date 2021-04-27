c8d775f8150fcb0e5091d2c245f32044c750a2c4
Vladislav Lukyanuk <vlukyanuk.job@gmail.com>
2021-04-27 16:51:55 +0600

    setup the node js debug

--- server/buildConfig/webpack-config-local.js
+++ server/buildConfig/webpack-config-local.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const Dotenv = require('dotenv');
 const { merge } = require('webpack-merge');
-const { DefinePlugin } = require('webpack');
+const NodemonPlugin = require('nodemon-webpack-plugin');
 
 const envConfig = Dotenv.config({ path: path.join(__dirname, 'env', '.env.local') }).parsed;
 
@@ -9,12 +9,13 @@ const baseConfig = require('./webpack-config-base');
 
 const config = {
     mode: 'development',
-    devtool: 'source-map'
-    // plugins: [
-    //     new DefinePlugin({
-    //         'process.env': JSON.stringify(envConfig),
-    //     })
-    // ],
+    devtool: 'source-map',
+    plugins: [
+        new NodemonPlugin()
+        // new DefinePlugin({
+        //     'process.env': JSON.stringify(envConfig),
+        // })
+    ],
 };
 
 module.exports = merge(baseConfig, config);
